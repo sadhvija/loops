8b99efcb524bf61f8ddc9762d26c8106cfa6153a
sadhvija <60653845+sadhvija@users.noreply.github.com>
2024-10-24 21:34:25 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,51 @@
+//for loop prints 0-5
+for(var a=0;a<=5;a++){
+    console.log(a)
+}
+//for loop prints reverse 10-6
+for(var b=10;b>5;b--)
+    console.log(b)
+//for loop prints 15-7 reverse
+for(var c=15;c>=7;c--){
+    console.log(c)
+}
+//While loop from 40-45
+var d=40
+while(d<=45){
+    console.log(d)
+    d++
+}
+//while loop in reverse order
+var e=96
+while(e>=91){
+    e--
+    console.log(e)
+}
+var f=33
+ while(f>=29){
+    f--
+    console.log(f)
+}
+//out of loop execution
+for(var g=0;g<=5;g++){
+
+}
+console.log(g)
+//
+var h=1
+while(h<4){
+    h++
+}
+console.log(h)
+//Do while increment
+var i=76
+do{
+    i++
+    console.log(i)
+}while(i<80)
+//Do while decrement
+var j=45
+do{
+    j--
+    console.log(j)
+}while(j>39)
